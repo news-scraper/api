4ab7042c46b800950ed8f50cb79da224f934470d
Julian Nadeau <julian@jnadeau.ca>
2016-09-21 22:41:12 -0400

    Add active link markers to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,15 @@
 module ApplicationHelper
   def navigation_links
-    @navigation_links ||= {
-      "Domains"  => { link: domains_path, icon: "globe" },
-      "Articles" => { link: news_articles_path, icon: "newspaper-o" },
-      "Training" => { link: training_logs_path, icon: "plus" },
-      "Queries"  => { link: scrape_queries_path, icon: "question" },
-      "Sign Out" => { link: destroy_user_session_path, icon: "sign-out", method: 'delete' },
-    }
+    @navigation_links ||= begin
+      links = {
+        "Domains"  => { link: domains_path, icon: "globe" },
+        "Articles" => { link: news_articles_path, icon: "newspaper-o" },
+        "Training" => { link: training_logs_path, icon: "plus" },
+        "Queries"  => { link: scrape_queries_path, icon: "question" },
+        "Sign Out" => { link: destroy_user_session_path, icon: "sign-out", method: 'delete' },
+      }
+      links.map { |_, link_info| link_info[:active] = current_page?(link_info[:link]) ? 'active' : '' }
+      links
+    end
   end
 end
