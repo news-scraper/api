db6210e837e3f3cb8b01d41ec8c8a1a8faaa3456
Julian Nadeau <julian@jnadeau.ca>
2016-09-23 18:21:51 -0400

    better columns

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ statuses = %w(untrained trained claimed).cycle
 
 domains.each do |domain|
   status = statuses.next
-  Rails.logger.info "Creating training_log for #{domain} with state #{status}"
+  puts "Creating training_log for #{domain} with state #{status}"
   TrainingLog.create(
     root_domain: domain,
     uri: "https://#{domain}/path",
@@ -13,28 +13,30 @@ domains.each do |domain|
 end
 
 queries.each do |query|
-  Rails.logger.info "Creating ScrapeQuery with query #{query}"
+  puts "Creating ScrapeQuery with query #{query}"
   scrape_query = ScrapeQuery.find_or_create_by(query: 'technology')
 
   domains.each do |domain|
-    Rails.logger.info "Creating news_article for #{domain}"
-    scrape_query.news_articles.create(
-      author: 'author',
-      body: 'body text goes here',
-      description: 'description goes here',
-      keywords: 'keywords_1,keywords_2',
-      section: 'section',
-      datetime: DateTime.now.utc,
-      title: "Title for #{domain}",
-      root_domain: domain,
-      uri: "https://#{domain}/path"
-    )
+    puts "Creating news_articles for #{domain}"
+    3.times do |i|
+      scrape_query.news_articles.create!(
+        author: 'author',
+        body: 'body text goes here',
+        description: (['description goes here'] * (1..20).to_a.sample).to_sentence,
+        keywords: 'keywords_1,keywords_2',
+        section: 'section',
+        datetime: DateTime.now.utc,
+        title: "Title for #{domain}",
+        root_domain: domain,
+        uri: "https://#{domain}/path/#{i}"
+      )
+    end
   end
 end
 
 data_types = NewsScraper.configuration.scrape_patterns['data_types']
 %w(shopify.ca).each do |domain|
-  Rails.logger.info "Making a domain entry for #{domain}"
+  puts "Making a domain entry for #{domain}"
   domain_entries_attributes = data_types.collect do |data_type|
     {
       data_type: data_type,
@@ -46,5 +48,5 @@ data_types = NewsScraper.configuration.scrape_patterns['data_types']
 end
 
 user_params = { email: 'email@example.com', password: 'password', password_confirmation: 'password' }
-Rails.logger.info "Making a default user - #{user_params}"
+puts "Making a default user - #{user_params}"
 User.create(user_params)
