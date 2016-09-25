a9181dbb2882ae887b3082b4889edc4c0e1836aa
Julian Nadeau <julian@jnadeau.ca>
2016-09-25 16:00:58 -0400

    Put order on news articles index

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -1,6 +1,6 @@
 class NewsArticlesController < ApplicationController
   def index
-    @news_articles = NewsArticle.all
+    @news_articles = NewsArticle.all.order(created_at: :desc)
   end
 
   def articles
