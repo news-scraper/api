133b75ec07e5c6b39860b6f6d366ebc3afac862e
Julian Nadeau <julian@jnadeau.ca>
2016-09-28 11:43:25 -0400

    Add profile to nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,6 +6,7 @@ module ApplicationHelper
         "Articles" => { link: news_articles_path, icon: "newspaper-o" },
         "Training" => { link: training_logs_path, icon: "plus" },
         "Queries"  => { link: scrape_queries_path, icon: "question" },
+        "Profile"  => { link: profile_path, icon: 'user' },
         "Sign Out" => { link: destroy_user_session_path, icon: "sign-out", method: 'delete' },
       }
       links.map { |_, link_info| link_info[:active] = current_page?(link_info[:link]) ? 'active' : '' }
