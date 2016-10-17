5edf5865d9989827bb273716f7e7ab8ec14f93ca
Julian Nadeau <julian@jnadeau.ca>
2016-10-17 01:11:57 -0400

    fix scraper_job

--- app/jobs/scraper_job.rb
+++ app/jobs/scraper_job.rb
@@ -8,21 +8,43 @@ class ScraperJob < ApplicationJob
       case a.class.to_s
       when "NewsScraper::Transformers::ScrapePatternNotDefined"
         Rails.logger.info "#{a.root_domain} was not trained"
-        unless TrainingLog.exists?(url: a.url)
-          params = {
-            root_domain: a.root_domain,
-            url: a.url,
-            scrape_query_id: scrape_query.id
-          }
-          params[:trained_status] = 'untrainable' if TrainingLog.find_by(root_domain: a.root_domain).try(:untrainable?)
-          TrainingLog.create!(params)
-        end
+        find_or_create_log(url: a.url, root_domain: a.root_domain, scrape_query: scrape_query)
       when "NewsScraper::ResponseError"
         Rails.logger.error "#{a.url} returned an error: #{a.error_code}-#{a.message}"
       else
-        Rails.logger.info "creating article for #{a[:url]}"
-        NewsArticle.create!(a.merge(scrape_query_id: scrape_query.id)) unless NewsArticle.exists?(url: a[:url])
+        Rails.logger.info "Creating article for #{a[:url]}"
+        if TrainingLog.exists?(root_domain: a[:root_domain])
+          Rails.logger.error "TrainingLog somehow didn't exist for #{a[:root_domain]}"
+          find_or_create_log(
+            url: a[:url],
+            root_domain: a[:root_domain],
+            scrape_query: scrape_query,
+            trained_status: 'trained'
+          )
+        end
+
+        unless NewsArticle.exists?(url: a[:url])
+          NewsArticle.create!(a.merge(scrape_query_id: scrape_query.id))
+        end
       end
+    end
+  end
+
+  def find_or_create_log(url:, root_domain:, scrape_query:, trained_status: 'untrained')
+    if TrainingLog.exists?(url: url)
+      TrainingLog.find_by(url: url)
+    else
+      params = {
+        root_domain: root_domain,
+        url: url,
+        scrape_query_id: scrape_query.id
+      }
+      params[:trained_status] = if TrainingLog.find_by(root_domain: root_domain).try(:untrainable?)
+        'untrainable'
+      else
+        trained_status
+      end
+      TrainingLog.create!(params)
     end
   end
 end
